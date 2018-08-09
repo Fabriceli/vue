f35155993dc63c2beaeb52ff10946f11f9b7a131
liyihao <li842162578@gmail.com>
2018-08-09 23:58:31 +0800

    新增新闻的增删改查

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
-let host = 'http://www.johnkee.com';
+let host = 'https://www.johnkee.com';
+
+// let host = 'http://127.0.0.1:8000';
 
 // 获取项目信息
-export const queryProjects = params => { return axios.get(`${host}/projects/`); };
-
-// get news infos
-export const getNews = params => { return axios.get(`${host}/news/`); };
+export const queryProjects = params => { return axios.get(`${host}/projects/`, params); };
 
 // get district infos
-export const getDistrict = params => { return axios.get(`${host}/regions/`); };
+export const getDistrict = params => { return axios.get(`${host}/regions/`, params); };
 
 // 获取项目类别信息
 export const getCategory = params => {
@@ -19,3 +18,15 @@ export const getCategory = params => {
         return axios.get(`${host}/industrytype/`, params);
     }
 };
+
+//添加新闻
+export const addNews = params => {return axios.post(`${host}/news/`, params)};
+
+//删除新闻
+export const delNews = newsId => {return axios.delete(`${host}/news/`+newsId+'/')};
+
+//修改新闻
+export const updateNews = (newsId, params) => {return axios.patch(`${host}/news/`+newsId+'/', params)};
+
+// get news infos
+export const getNews = params => { return axios.get(`${host}/news/`, params); };
